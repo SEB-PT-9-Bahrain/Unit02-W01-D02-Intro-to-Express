ff5650ca32a075d6e241b95280679e80337e1cd6
omarakamal <omaraakamal@gmail.com>
2026-08-24 19:21:44 +0300

    update

--- server.js
+++ server.js
@@ -5,12 +5,6 @@ const app = express() // creates a instance of server
 // app.get() has 2 arguments
 // 1. the route: like /ali, /about, /hi
 // 2. the function that will run when we get the request
-
-app.get('/yuseph',(request,response)=>{
-    
-})
-
-
 app.get('/ali',(request, response)=>{
     console.log('Request sent to Ali Route')
     response.send('<h1>First route success</h1>')
