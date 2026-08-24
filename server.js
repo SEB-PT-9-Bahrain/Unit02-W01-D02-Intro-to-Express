c74ff90d1a6e31a6f1d13c630a913dee53af1da0
omarakamal <omaraakamal@gmail.com>
2026-08-24 19:39:50 +0300

    update

--- server.js
+++ server.js
@@ -5,10 +5,18 @@ const app = express() // creates a instance of server
 // app.get() has 2 arguments
 // 1. the route: like /ali, /about, /hi
 // 2. the function that will run when we get the request
+console.log(__dirname)
 app.get('/ali',(request, response)=>{
     console.log('Request sent to Ali Route')
-    response.send('<h1>First route success</h1>')
+    response.sendFile(__dirname + '/views/student.html')
 })
+
+
+// Exercise 1:
+// 1. Create a about-me.html file in your views folder
+// 2. add the technologies you learned in this class inside of the file
+// 3. make new app.get() route on /about
+// 4. if a get request is sent to /about it should send back the about-me.html file
 
 
 
