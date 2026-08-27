bb9e2b5a3489fff67d27c3c32ba0d9ff335775f3
omarakamal <omaraakamal@gmail.com>
2026-08-27 09:10:06 +0300

    update

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(express.static('public'))
 // 1. the route: like /ali, /about, /hi
 // 2. the function that will run when we get the request
 console.log(__dirname)
+
+
 app.get('/',(request, response)=>{
     console.log('Request sent to Ali Route')
     response.sendFile(__dirname + '/views/student.html')
